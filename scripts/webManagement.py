91351703223408265d5c110a5fb9e56b9b51160c
dajman <dajmanen@gmail.com>
2023-11-27 11:26:15 +0100

    before updating

--- scripts/webManagement.py
+++ scripts/webManagement.py
@@ -17,10 +17,9 @@ class WebManagement:
         self.driver.find_element(By.ID, "ext-comp-2349__Buildings").click()
         self.driver.maximize_window()
         self.loadedFramework = False
-        #self.wait = WebDriverWait(self.driver, 10)
-        self.run()
+        self.wait = WebDriverWait(self.driver, 10)
 
-    def loadLevel(self):
+    def uploadLevel(self):
         # self.wait.until(EC.element_to_be_clickable((By.XPATH, "//td[@class='x-grid3-col x-grid3-cell x-grid3-td-5 scada-cell-down scada-cell-icon ']/following-sibling::td"))).click()
         self.driver.find_element(By.XPATH, "//td[@class='x-grid3-col x-grid3-cell x-grid3-td-5 scada-cell-down scada-cell-icon ']/following-sibling::td").click()
         sleep(0.2)
@@ -35,7 +34,7 @@ class WebManagement:
         self.driver.find_element(By.ID, "buildings-import-submit").click()
         sleep(1)
 
-    def loadFramework(self):
+    def uploadFramework(self):
 
         self.driver.find_element(By.ID, "ext-comp-1651__ext-comp-1606").click()
         sleep(0.2)
@@ -52,7 +51,7 @@ class WebManagement:
         self.loadedFramework = True
         sleep(1)
 
-    def loadWidgets(self, ):
+    def uploadWidgetsInOrder(self, ):
         if not self.loadedFramework:
             pass
 
@@ -115,16 +114,51 @@ class WebManagement:
             self.driver.find_element(By.XPATH, "//table[@id='buildings-import-submit']").click()
             sleep(0.5)
 
+    def uploadInfoWidgets(self):
+        for widget in self.infoWidgets:
+            self.driver.find_element(By.ID, "ext-comp-1651__ext-comp-1606").click()
+            sleep(0.5)
+            self.driver.find_element(By.XPATH, "//div[@data-record-id='widget']").click()
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//table[@id='ext-comp-1664']").click()
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//input[@value='keep']").click()
+            sleep(0.2)
+            choose_file = self.driver.find_element(By.XPATH, "//input[@accept='.tar']")
+            choose_file.send_keys(
+                r"C:\Users\Damian\PycharmProjects\KNX-widget-factory\output\widgets" + "\\Info_Widget_Rom-" + widget + ".tar")
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//table[@id='buildings-import-submit']").click()
+            sleep(0.5)
+
+    def uploadTrendWidgets(self):
+        for widget in self.trendWidgets:
+            self.driver.find_element(By.ID, "ext-comp-1651__ext-comp-1606").click()
+            sleep(0.5)
+            self.driver.find_element(By.XPATH, "//div[@data-record-id='widget']").click()
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//table[@id='ext-comp-1664']").click()
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//input[@value='keep']").click()
+            sleep(0.2)
+            choose_file = self.driver.find_element(By.XPATH, "//input[@accept='.tar']")
+            choose_file.send_keys(
+                r"C:\Users\Damian\PycharmProjects\KNX-widget-factory\output\widgets" + "\\Trend_Widget_Rom-" + widget + ".tar")
+            sleep(0.2)
+            self.driver.find_element(By.XPATH, "//table[@id='buildings-import-submit']").click()
+            sleep(0.5)
+
     def isEnable(self):
         pass
 
-    def run(self):
+    def upload(self):
         sleep(1)
         if self.levelName is not None:
             self.levelName = self.levelName.replace(".yml", ".tar")
-            self.loadLevel()
+            self.uploadLevel()
         if self.frameworkName is not None:
             self.frameworkName = self.frameworkName.replace(".yml", ".tar")
             self.loadFramework()
         self.loadWidgets()
+
 
